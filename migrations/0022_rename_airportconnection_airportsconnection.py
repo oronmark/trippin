b88f819369be6797b157a7c774f87f7932374afd
Vicki <vicki.kalm@gmail.com>
2022-03-20 17:31:07 +0200

    fixed wtf

--- migrations/0022_rename_airportconnection_airportsconnection.py
+++ migrations/0022_rename_airportconnection_airportsconnection.py
@@ -11,7 +11,7 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.RenameModel(
-            old_name='AirportsConnection',
+            old_name='AirportConnection',
             new_name='AirportsConnection',
         ),
     ]
